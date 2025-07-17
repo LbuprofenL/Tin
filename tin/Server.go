7a6340c78e881309b2bf264f6f17b5cce75ca68d
qiansi <qiansi.lxl@oceanbase.com>
2025-07-17 15:15:04 +0800

    feat: tin v0.2 - connection

--- tin/Server.go
+++ tin/Server.go
@@ -7,7 +7,7 @@ import (
 // Server 模块的测试函数
 func main() {
 	// 1 创建一个server 句柄 s
-	s := tnet.NewServer("[tin V0.1]")
+	s := tnet.NewServer("[tin V0.2]")
 
 	// 2 开启服务
 	s.Serve()
--- tin/tnet/server.go
+++ tin/tnet/server.go
@@ -1,6 +1,7 @@
 package tnet
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"time"
@@ -15,6 +16,19 @@ type Server struct {
 	IP        string
 	Port      int
 }
+
+// CallBackToClient 定义当前客户端链接的handle api
+func CallBackToClient(conn *net.TCPConn, data []byte, cnt int) error {
+	// 回显业务
+	fmt.Println("[Conn Handle] CallBackToClient ... ")
+	if _, err := conn.Write(data[:cnt]); err != nil {
+		fmt.Println("write back buf err ", err)
+		return errors.New("CallBackToClient error")
+	}
+	return nil
+}
+
+// ============== 实现tinface.IServer 里的全部接口方法 ===========
 
 func (s *Server) Start() {
 	fmt.Println("[START] Server is starting...")
@@ -36,7 +50,7 @@ func (s *Server) Start() {
 
 		// 已经监听成功
 		fmt.Println("start Tin server  ", s.Name, " succ, now listenning...")
-
+		var cid uint32
 		// 3 启动server网络连接业务
 		for {
 			// 3.1 阻塞等待客户端建立连接请求
@@ -49,24 +63,9 @@ func (s *Server) Start() {
 			// 3.2 TODO Server.Start() 设置服务器最大连接控制,如果超过最大连接，那么则关闭此新的连接
 
 			// 3.3 TODO Server.Start() 处理该新连接请求的 业务 方法， 此时应该有 handler 和 conn是绑定的
-
-			// 我们这里暂时做一个最大512字节的回显服务
-			go func() {
-				// 不断的循环从客户端获取数据
-				for {
-					buf := make([]byte, 512)
-					cnt, err := conn.Read(buf)
-					if err != nil {
-						fmt.Println("recv buf err ", err)
-						continue
-					}
-					// 回显
-					if _, err := conn.Write(buf[:cnt]); err != nil {
-						fmt.Println("write back buf err ", err)
-						continue
-					}
-				}
-			}()
+			dealConn := NewConntion(conn, cid, CallBackToClient)
+			cid++
+			go dealConn.Start()
 		}
 	}()
 }
